2fb29750b5de6e52a2e0733284af72e9572fcec8
ashritha-uddisapu <uddisapuashritha@gmail.com>
2026-09-02 19:57:41 +0530

    Implemented image steganography encoding and decoding

---
+++ decoder.py
@@ -0,0 +1,41 @@
+from PIL import Image
+image=Image.open("outputs/test1.png")
+print(image.size)
+print(image.mode)
+
+def decode(image):
+
+    sign_bits = ""
+    sign = ""
+    payload_length = 0
+    length_bits = ""
+    secret_index = 0
+    payload_binary = ""
+    payload = ""
+
+    for y in range(image.height):
+        for x in range(image.width):
+            pixel=image.getpixel((x,y))
+            for i in range(3):
+                if secret_index<32:
+                    sign_bits += str(pixel[i] & 1)
+                    if len(sign_bits)==32:
+                        for i in range (0,32,8):
+                            sign += chr(int(sign_bits[i:i+8],2))
+                if secret_index>=32 and secret_index<64:
+                    if sign == "IRIS":
+                        length_bits += str(pixel[i] & 1)
+                if secret_index==64:
+                    payload_length = int(length_bits,2)
+                    end_limit = payload_length + 64 - 1
+                if secret_index>=64 and secret_index<=end_limit:
+                    payload_binary += str(pixel[i] & 1)
+                    flag2 = True
+                secret_index = secret_index+1
+
+    for i in range(0,payload_length,8):
+        payload += chr(int(payload_binary[i:i+8],2))
+
+    return payload
+
+print(decode(image))
---
+++ encoder.py
@@ -0,0 +1,50 @@
+from PIL import Image
+def encode(image,message):
+    converted=''.join(format(ord(char),'08b')for char in message) #8-bits storage per character; 8-bit representation of a character; binary representation
+    payload_length=len(converted) #normal int
+    sign = "IRIS"
+    iris_sign = ''.join(format(ord(char),'08b') for char in sign) #32-bits but in 8-bit, like IRIS = 4 char * 8 bits = 32 bits in total
+    payload_length_bits = format(payload_length,'032b') #convert length from decimal to 32-bit format n store it (for higher numbers if the length of the payload is more); also the decoder knows where the length field ends, exactly 32 bits cause length is represented in 32-bit format
+    encoded_bits = iris_sign + payload_length_bits + converted #variable total, holds everything together
+    image_capacity=image.height*image.width*3
+    if image_capacity<len(encoded_bits):
+        return "Message is too large for this image"
+    else:
+        secret_index=0
+        for y in range(image.height):
+            for x in range(image.width): #iterate through pixels
+                pixel=image.getpixel((x,y))
+                pixel=list(pixel) #converting tuple to list for modification
+                for i in range(3): #iterate through the rgb channel (1+1+1, so rnage(3))
+                    if secret_index<len(encoded_bits):
+                        secret_bit=int(encoded_bits[secret_index])
+                        if (pixel[i] & 1)!=secret_bit:
+                            if secret_bit==0:
+                                pixel[i]=pixel[i] & ~1 #force 0
+                            else:
+                                pixel[i]=pixel[i] | 1 #force 1
+                        secret_index+=1 #move to the next index
+                pixel=tuple(pixel) #converting list to tuple for changing the pixes
+                image.putpixel((x,y),pixel) #putting back the changed pixels
+                if secret_index == len(encoded_bits):
+                    print("Message encoded!")
+                    return image
+    return image
+
+image = Image.open("uploads/test.jpeg")
+encoded_image=encode(image,"Hiiiiiii, this is Ashritha, trying to encode this image :)")
+encoded_image.save("outputs/test1.png")
+
+
+
+#payload header and metadata
+#message = input("Enter the message: ")
+
+# print(f"Encoded bits (sign + payload_length + payload) = ",encoded_bits)
+
+#Example for payload header format:
+                # ┌────────────────┬────────────────┬─────────────────┐
+                # │ IRIS           │ LENGTH = 16    │ "hi"            │      
+                # │ 32 bits        │ 32 bits        │ 16 bits         │
+                # └────────────────┴────────────────┴─────────────────┘
+#length is 16 because "hi" in 8-bit representation, so 8+8=16 and that 16 is represented in 32-bit

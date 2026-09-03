852b44062b372621e501cd6144709e8e3b596c27
ashritha-uddisapu <uddisapuashritha@gmail.com>
2026-09-03 09:03:17 +0530

    Added user input for image encoding

--- decoder.py
+++ decoder.py
@@ -17,20 +17,28 @@ def decode(image):
         for x in range(image.width):
             pixel=image.getpixel((x,y))
             for i in range(3):
+
                 if secret_index<32:
                     sign_bits += str(pixel[i] & 1)
+
                     if len(sign_bits)==32:
                         for i in range (0,32,8):
                             sign += chr(int(sign_bits[i:i+8],2))
+
+                        if sign != "IRIS":
+                            return "IRIS Sign not found! "
+
                 if secret_index>=32 and secret_index<64:
                     if sign == "IRIS":
                         length_bits += str(pixel[i] & 1)
+
                 if secret_index==64:
                     payload_length = int(length_bits,2)
                     end_limit = payload_length + 64 - 1
+
                 if secret_index>=64 and secret_index<=end_limit:
                     payload_binary += str(pixel[i] & 1)
-                    flag2 = True
+
                 secret_index = secret_index+1
 
     for i in range(0,payload_length,8):
